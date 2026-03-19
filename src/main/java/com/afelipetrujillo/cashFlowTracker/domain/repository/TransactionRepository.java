0808eb2bb13d76a8148c73367f3eb369c54f63cb
AFelipeTrujillo <andres.trujillo.1@gmail.com>
2026-03-19 12:16:45 +0100

    Define TransactionRepository

---
+++ src/main/java/com/afelipetrujillo/cashFlowTracker/domain/repository/TransactionRepository.java
@@ -0,0 +1,23 @@
+package com.afelipetrujillo.cashFlowTracker.domain.repository;
+
+import com.afelipetrujillo.cashFlowTracker.domain.model.Transaction;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TransactionRepository {
+
+    // Save or update a Transaction
+    Transaction save(Transaction transaction);
+
+    // Return a Transaction by id
+    Optional<Transaction> findById(UUID id);
+
+    // Return a list with all Transactions
+    List<Transaction> findAll();
+
+    // Delete by id
+    void deleteById(UUID id);
+}

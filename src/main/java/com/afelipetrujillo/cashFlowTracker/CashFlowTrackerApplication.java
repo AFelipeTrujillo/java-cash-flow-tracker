c9e6dcda9259e0ac583fea9fcc70594560bbf8b8
AFelipeTrujillo <andres.trujillo.1@gmail.com>
2026-03-19 11:17:32 +0100

    First commit with the installation packages and dependencies

---
+++ src/main/java/com/afelipetrujillo/cashFlowTracker/CashFlowTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.afelipetrujillo.cashFlowTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afelipetrujillo/cashFlowTracker/CashFlowTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afelipetrujillo.cashFlowTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

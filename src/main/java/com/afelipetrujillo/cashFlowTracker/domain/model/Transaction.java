ce9cc1b5a8b974a198204f7dae77c8befa4a8102
AFelipeTrujillo <andres.trujillo.1@gmail.com>
2026-03-19 12:16:19 +0100

    Fix type

--- src/main/java/com/afelipetrujillo/cashFlowTracker/domain/model/Transaction.java
+++ src/main/java/com/afelipetrujillo/cashFlowTracker/domain/model/Transaction.java
@@ -21,15 +21,15 @@ public class Transaction {
     private String description;
     private BigDecimal amount;
     private TransactionType type;
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     public void initialize() {
         if (this.id == null) {
             this.id = UUID.randomUUID();
         }
 
-        if (this.createAt == null) {
-            this.createAt = LocalDateTime.now();
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
         }
     }
 
